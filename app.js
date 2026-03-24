fab866dcb5955bfc611b54b6aac1ea1fd2db6bd5
Abdelrahman Tarek <101576496+abdobary@users.noreply.github.com>
2026-03-24 10:37:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // Database connection
 async function dbConnection() {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("✅ Connected to MongoDB!");
     } catch (err) {
         console.log("❌ MongoDB connection error:", err.message);
