4b9786e4f68c1326806d8ffdef9769e111c71b7a
Abdelrahman Tarek <101576496+abdobary@users.noreply.github.com>
2026-03-24 11:48:45 +0200

    Update authRoutes.js

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,10 +1,8 @@
-const express = require("express")
-
+const express = require("express");
 const router = express.Router();
+const { registerController, loginController } = require("../Controllers/authController");
 
-const {registerController,loginController} = require("../Controllers/authController")
+router.post("/register", registerController);
+router.post("/login", loginController);
 
-router.post("/register", registerController)
-router.post("/login", loginController)
-
-module.exports=router;
+module.exports = router;
