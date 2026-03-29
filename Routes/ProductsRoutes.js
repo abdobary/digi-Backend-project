c1608710b5db868d8170f4d22584c674b24f6054
Abdelrahman Tarek <101576496+abdobary@users.noreply.github.com>
2026-03-29 16:31:50 +0200

    Update ProductsRoutes.js

--- Routes/ProductsRoutes.js
+++ Routes/ProductsRoutes.js
@@ -11,12 +11,10 @@ const {
 } = require("../Controllers/ProductController");
 const { protect } = require('../middleware/authMiddleware');
 
-// Public routes
 router.get('/', getProducts);
 router.get('/:id', getProductById);
 router.get('/user/:userId', getProductsByUser);
 
-// Protected routes (require login)
 router.post('/', protect, createProduct);
 router.put('/:id', protect, updateProduct);
 router.delete('/:id', protect, deleteProduct);
